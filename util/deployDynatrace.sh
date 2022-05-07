1d6ee15fba6e35028f97014b4121206840e9acf1
iomegak12 <jd.ramkumar@gmail.com>
2022-05-07 11:35:50 +0530

    another commit

--- util/deployDynatrace.sh
+++ util/deployDynatrace.sh
@@ -35,7 +35,7 @@ deployOneAgent() {
 }
 
 deployApplicationConfig() {
-	cd Workshop-BizOps/util
+	# cd ~/dt-workshop-bizops/util
 
 	# export PUBLIC_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
 	export PRIVATE_HOSTNAME=$(hostname)
